768a1dad99897d23f6d2e474ab11026082f8d5a2
kimsooyoung <tge1375@naver.com>
2022-02-11 21:23:00 +0900

    Fix : output screen

--- kalman_filter/launch/launch_auxiliary_nodes.launch.py
+++ kalman_filter/launch/launch_auxiliary_nodes.launch.py
@@ -88,7 +88,6 @@ def generate_launch_description():
 
     move_forward = ExecuteProcess(
         cmd=['ros2', 'topic', 'pub', '/cmd_vel', 'geometry_msgs/msg/Twist', '{linear: {x: 0.5, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}'],
-        output='screen'
     )
 
     # rqt robot steering
